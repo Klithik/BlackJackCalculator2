a58113009ea8213dafd582dde7e9bcbe01923447
Klithik <santiago.silva@mayor.cl>
2022-05-28 21:47:06 -0400

    alo

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ layout = [
     sg.Button('5',key = '5Player'),sg.Button('6',key = '6Player'),sg.Button('7',key = '7Player'),sg.Button('8',key = '8Player'),
     sg.Button('9',key = '9Player'),sg.Button('10',key = '10Player'),sg.Button('J',key = 'JPlayer'),sg.Button('Q',key = 'QPlayer'),
     sg.Button('K',key = 'KPlayer'),sg.Button('Split',key = 'split')],
-    [sg.Text('Estado Jugador: 0',key = 'pjePlayer'), sg.Text('Estado Dealer: 0',key = 'pjeDealer'),sg.Button('Cambio de ronda',key = 'ronda')],
+    [sg.Text('Estado Jugador: 0',key = 'pjePlayer'), sg.Text('Estado Dealer: 0',key = 'pjeDealer'),sg.Text('',key = 'pje2Player'),
+    sg.Button('Cambio de ronda',key = 'ronda')],
     [sg.Text('Conteo de cartas: ',key = 'cuenta')]
 ]
 
@@ -79,6 +80,95 @@ mano2Player = [
     [0,12],
     [0,13]]
 
+#==========ZONA DE METODOS==========
+def PuntajeDealer():
+    suma = 0
+    for t in manoDealer:
+        if t[1] > 9:
+            suma += t[0]*10
+        else:
+            suma += t[0]*t[1]
+    if suma < 12 and manoDealer[0][0] != 0:
+        suma += 10
+    window['pjeDealer'].update('Estado Dealer: ' + str(suma))
+    return suma
+
+def PuntajePlayer():
+    suma = 0
+    for t in manoPlayer:
+        if t[1] > 9:
+            suma += t[0]*10
+        else:
+            suma += t[0]*t[1]
+    if suma < 12 and manoPlayer[0][0] != 0:
+        suma += 10
+    window['pjePlayer'].update('Estado Jugador: ' + str(suma))
+    return suma
+
+def Puntaje2Player():
+    suma = 0
+    for t in mano2Player:
+        if t[1] > 9:
+            suma += t[0]*10
+        else:
+            suma += t[0]*t[1]
+    if suma < 12 and mano2Player[0][0] != 0:
+        suma += 10
+    window['pje2Player'].update('Estado 2 Jugador: ' + str(suma))
+    return suma
+
+def cuentaCartas():
+    conteo = 0
+    for t in manoDealer:
+        num = t[1]
+        cantidad = t[0]
+        if num > 1 and num < 7:
+            conteo -= cantidad
+        elif num > 9 or num == 1:
+            conteo += cantidad
+    
+    for t in manoPlayer:
+        num = t[1]
+        cantidad = t[0]
+        if num > 1 and num < 7:
+            conteo -= cantidad
+        elif num > 9 or num == 1:
+            conteo += cantidad
+    return conteo
+
+def probSacar(num):
+    return baraja[num-1][0]/cartasRestantes()
+
+def nuevaCarta(carta,mano):
+    baraja[carta-1][0] -= 1
+    if mano == 1:
+        manoDealer[carta-1][0] += 1
+    elif mano == 2:
+        manoPlayer[carta-1][0] += 1
+    
+    window['cuenta'].update('Conteo de cartas: ' + str(cuentaCartas()))
+    PuntajeDealer()
+    PuntajePlayer()
+
+def cartasRestantes():
+    suma = 0
+    for t in baraja:
+        suma += t[0]
+    return suma
+
+def splitAs():
+    total = 0
+    repetido = 0
+    for t in manoPlayer:
+        total += t[0]
+        if t[0] == 2:
+            repetido = t[1]
+    if total == 2:
+        manoPlayer[repetido-1][0] -= 1
+        mano2Player[repetido-1][0] += 1
+    Puntaje2Player()
+    PuntajePlayer()
+
 while True:
     event,values = window.read()
 
@@ -90,6 +180,7 @@ while True:
             n = values[0]
             window['mazos'].update('Mazos en uso: ' + str(n))
     
+    #ACCIONAMIENTO BOTON DE CAMBIO DE RONDA
     if event == 'ronda':
         for t in manoDealer:
             t[0] = 0
@@ -98,77 +189,8 @@ while True:
         window['pjeDealer'].update('Estado Dealer: 0')
         window['pjePlayer'].update('Estado Jugador: 0')
     
-    def cartasRestantes():
-        suma = 0
-        for t in baraja:
-            suma += t[0]
-        return suma
-    
-    def splitAs():
-        for t in manoPlayer:
-            if t[0] > 1:
-                print('a')
-    
     if event == 'split':
         splitAs()
-    
-    def PuntajeDealer():
-        suma = 0
-        for t in manoDealer:
-            if t[1] > 9:
-                suma += t[0]*10
-            else:
-                suma += t[0]*t[1]
-        if suma < 12 and manoDealer[0][0] != 0:
-            suma += 10
-        window['pjeDealer'].update('Estado Dealer: ' + str(suma))
-        return suma
-    
-    def PuntajePlayer():
-        suma = 0
-        for t in manoPlayer:
-            if t[1] > 9:
-                suma += t[0]*10
-            else:
-                suma += t[0]*t[1]
-        if suma < 12 and manoPlayer[0][0] != 0:
-            suma += 10
-        window['pjePlayer'].update('Estado Jugador: ' + str(suma))
-        return suma
-    
-    def cuentaCartas():
-        conteo = 0
-        for t in manoDealer:
-            num = t[1]
-            cantidad = t[0]
-            if num > 1 and num < 7:
-                conteo -= cantidad
-            elif num > 9 or num == 1:
-                conteo += cantidad
-        
-        for t in manoPlayer:
-            num = t[1]
-            cantidad = t[0]
-            if num > 1 and num < 7:
-                conteo -= cantidad
-            elif num > 9 or num == 1:
-                conteo += cantidad
-
-        return conteo
-    
-    def probSacar(num):
-        return baraja[num-1][0]/cartasRestantes()
-    
-    def nuevaCarta(carta,mano):
-        baraja[carta-1][0] -= 1
-        if mano == 1:
-            manoDealer[carta-1][0] += 1
-        elif mano == 2:
-            manoPlayer[carta-1][0] += 1
-        
-        window['cuenta'].update('Conteo de cartas: ' + str(cuentaCartas()))
-        PuntajeDealer()
-        PuntajePlayer()
     
     #ACCIONES DE BOTONES DE SELECCION DE CARTAS
     if event == 'AsDealer':
